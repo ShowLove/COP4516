0534a670839d2d5686fdcca02ffb9cdecf586152
Carlos Andres Garzon <garzoncarlosandres@gmail.com>
2016-02-13 20:06:43 -0500

    Update processes.java

--- processes.java
+++ processes.java
@@ -1,37 +1,46 @@
-import java.util.Arrays;
-import java.io.*;
-import java.util.*;
+	import java.util.Arrays;
+	import java.io.*;
+	import java.util.*;
 
-public class processes
-{
-	public static void main( String args[] )
+	public class processes
 	{
-		processes();
+		public static void main( String args[] )
+		{
+			processes();
+		}
+
+		public static void processes()
+		{
+	       try 
+	       {
+	            Scanner input = new Scanner(System.in);
+	            System.out.print("Enter the file name with extention : ");
+	            File file = new File(input.nextLine());
+
+	            input = new Scanner(file);
+
+
+	            while (input.hasNextLine()) 
+	            {
+	                String line = input.nextLine();
+	                String[] values = line.split(" ");
+	                for(int i = 0; i < values.length; i++)
+	                {
+	                	if(values[i].equals("#"))
+	                		break;
+	                	System.out.print(" S"+values[i]+"E ");
+	                }
+	                System.out.println();
+	                //System.out.print(line);
+	                System.out.println();	      
+	            }
+	            input.close();
+
+	        } 
+	        catch (Exception ex) 
+	        {
+	            ex.printStackTrace();
+	        }
+
+		}
 	}
-
-	public static void processes()
-	{
-       try 
-       {
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter the file name with extention : ");
-            File file = new File(input.nextLine());
-
-            input = new Scanner(file);
-
-
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println(line);
-            }
-            input.close();
-
-        } 
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
-        }
-// 		System.out.println(N+","+X);
-
-	}
-}

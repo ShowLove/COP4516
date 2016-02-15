12c52fba3734bb82bd5b75cdad6f6b2905a049b0
Carlos Andres Garzon <garzoncarlosandres@gmail.com>
2016-02-14 22:39:18 -0500

    Update processes.java

--- processes.java
+++ processes.java
@@ -4,6 +4,8 @@
 
 	public class processes
 	{
+		public static int[][] arivalBurst;
+
 		public static void main( String args[] )
 		{
 			processes();
@@ -20,36 +22,67 @@
 	            input = new Scanner(file);
 
 	            int processCount = 0;
+	            int numProcess = 0;
 	            int runFor = 0;
 	            String use = new String("");
+	            String numProcessStr = new String("");
+	            char numChar = 'x';
+	            int arrival = 'A';
+	            int burst = 'B';
 	            //String str = new String("SOME")
 	            int quantum = 0;
 
 
-	            while (input.hasNextLine()) 
-	            {
-	                String line = input.nextLine();
-	                String[] values = line.split(" ");
-	                for(int i = 0; i < values.length; i++)
-	                {
-	                	if(values[i].equals("#"))
-	                		break;
-	                	if(values[i].equals("processcount"))
-	                		processCount = Integer.parseInt( values[i+1] );
-	                	if(values[i].equals("runfor"))
-	                		runFor = Integer.parseInt( values[i+1] );
+		        while (input.hasNextLine()) 
+		        {
+		            String line = input.nextLine();
+		            String[] values = line.split(" ");
+		            for(int i = 0; i < values.length; i++)
+		            {
+		            	if(values[i].equals("#"))
+		            		break;
+		            	if(values[i].equals("processcount"))
+		            	{
+		              		//if x is process number
+		            		//x,0--> Arrival //x,1--> burst
+		            		processCount = Integer.parseInt( values[i+1] );		            		
+		            		arivalBurst = new int[processCount+1][2];
+		            	}
+		            	if(values[i].equals("runfor"))
+		            		runFor = Integer.parseInt( values[i+1] );
 						if(values[i].equals("use"))
 						{	
-	                		use = values[i+1];
+		            		use = values[i+1];
 						}
-	                	if(values[i].equals("quantum"))
-	                		quantum = Integer.parseInt( values[i+1] );                			               
-	                	//System.out.print(" S"+values[i]+"E ");
-	                }
-	      
-	            }
-	            System.out.println("p_"+processCount+" r_"+runFor+" u_"+use+" q_"+quantum);
+		            	if(values[i].equals("quantum"))
+		            		quantum = Integer.parseInt( values[i+1] );
+		            	if(values[i].equals("name"))
+		            	{
+		            		//get process number as int
+		            		/*numProcessStr = values[i+1];
+		            		numChar = numProcessStr.charAt(1);
+		            		processCount = Character.getNumericValue(numChar);*/
+
+		            		//Number of Process element
+		            		processCount = Character.getNumericValue( values[i+1].charAt(1) );
+		            		//Arrival
+		            		arrival = Character.getNumericValue( values[i+3].charAt(0) );
+		            		//burst
+		            		burst = Character.getNumericValue( values[i+5].charAt(0) );
+
+		            		arivalBurst[processCount][0] = arrival;
+		            		arivalBurst[processCount][1] = burst;           		
+		            	}	                		               			               
+		            }
+		  
+		        } //End of while loop
+
+	            System.out.println("p_"+processCount+" r_"+runFor+" u_"+use+" q_"+quantum+" proCount_"+processCount);
+	            System.out.println(" arrival_"+arrival+" burst_"+burst);
+
+	            System.out.println(Arrays.deepToString(arivalBurst));
 	            input.close();
+	            //process information here
 
 	        } 
 	        catch (Exception ex) 
